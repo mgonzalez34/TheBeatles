c5f93731531ab683bee13a3126b0925d28374b58
mgonzalez34 <72830925+mgonzalez34@users.noreply.github.com>
2024-09-08 17:46:19 -0400

    Add files via upload

---
+++ restData.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    var data = {
+        products: [
+            { id: 1, name: "The Beatles Abbey Road", category: "Tee", price: 15.00 },
+            { id: 2, name: "The Beatles Singles Collection", category: "Ceramic Mug", price: 14.99 },
+            { id: 3, name: "The Beatles Crossing Over", category: "Sculpture", price: 399.99 },
+            { id: 4, name: "The Beatles Abbey Road", category: "Shopper Tote", price: 8.97 },
+            { id: 5, name: "The Beatles All You Need Is Love", category: "Sweatshirt", price: 8.56 },
+        ]
+    }
+    return data
+}
